bcdfded142f2bffbcc1c2abdf7810855c4afecb9
CalebeLouGer <calebelourenco581@gmail.com>
2025-11-17 16:25:29 -0400

    Ainda não finalizado | Consumo de API e Menu para interação com o Usuario

---
+++ public/src/br/com/classe/principal/ConversorDeMoedas.java
@@ -0,0 +1,10 @@
+package br.com.classe.principal;
+
+import java.util.Scanner;
+
+public class ConversorDeMoedas {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+    }
+}
